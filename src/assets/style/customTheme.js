9d3bfe90f5484ea4b899b14b1154d0a0d330d738
j2hyun <w2gus3263@gmail.com>
2022-04-25 15:49:59 +0900

    system color 적용

--- src/assets/style/customTheme.js
+++ src/assets/style/customTheme.js
@@ -1,125 +1,72 @@
-import { alpha, createTheme, lighten, darken } from "@mui/material";
-const themeColors = {
-  primary: "#07857F",
-  secondary: "#6E759F",
-  success: "#44D600",
-  warning: "#FFA319",
-  error: "#FF1943",
-  info: "#33C2FF",
-  black: "#2C2E38",
-  white: "#ffffff",
-  primaryAlt: "#000C57",
-};
-
+import { createTheme } from "@mui/material";
 const colors = {
-  gradients: {
-    blue1: "linear-gradient(135deg, #6B73FF 0%, #000DFF 100%)",
-    blue2: "linear-gradient(135deg, #ABDCFF 0%, #0396FF 100%)",
-    blue3: "linear-gradient(127.55deg, #141E30 3.73%, #243B55 92.26%)",
-    orange1: "linear-gradient(135deg, #FCCF31 0%, #F55555 100%)",
-    orange2: "linear-gradient(135deg, #FFD3A5 0%, #FD6585 100%)",
-    purple1: "linear-gradient(135deg, #43CBFF 0%, #9708CC 100%)",
-    pink1: "linear-gradient(135deg, #F6CEEC 0%, #D939CD 100%)",
-    pink2: "linear-gradient(135deg, #F761A1 0%, #8C1BAB 100%)",
-    green1: "linear-gradient(135deg, #FFF720 0%, #3CD500 100%)",
-    black1: "linear-gradient(100.66deg, #434343 6.56%, #000000 93.57%)",
+  ci: {
+    red: "#EA002C",
+    orange: "#F47725",
   },
-  shadows: {
-    success:
-      "0px 1px 4px rgba(68, 214, 0, 0.25), 0px 3px 12px 2px rgba(68, 214, 0, 0.35)",
-    error:
-      "0px 1px 4px rgba(255, 25, 67, 0.25), 0px 3px 12px 2px rgba(255, 25, 67, 0.35)",
-    info: "0px 1px 4px rgba(51, 194, 255, 0.25), 0px 3px 12px 2px rgba(51, 194, 255, 0.35)",
-    primary:
-      "0px 1px 4px rgba(85, 105, 255, 0.25), 0px 3px 12px 2px rgba(85, 105, 255, 0.35)",
-    warning:
-      "0px 1px 4px rgba(255, 163, 25, 0.25), 0px 3px 12px 2px rgba(255, 163, 25, 0.35)",
-    card: "0px 9px 16px rgba(159, 162, 191, 0.18), 0px 2px 2px rgba(159, 162, 191, 0.32)",
-    cardSm:
-      "0px 2px 3px rgba(159, 162, 191, 0.18), 0px 1px 1px rgba(159, 162, 191, 0.32)",
-    cardLg:
-      "0 5rem 14rem 0 rgb(255 255 255 / 30%), 0 0.8rem 2.3rem rgb(0 0 0 / 60%), 0 0.2rem 0.3rem rgb(0 0 0 / 45%)",
+  status: {
+    new: "#F5FFE8",
+    success: "#67C23A",
+    ready: "#F9AD3A",
+    warning: "#FFE91F",
+    danger: "#F65858",
+    cancel: "#C4C5CE",
+    done: "#178CF8",
   },
-  layout: {
-    general: {
-      bodyBg: "#f2f5f9",
+  fn: {
+    cancel: {
+      default: "#C4C5CE",
+      hover: "#B4B5BE",
     },
-    sidebar: {
-      background: themeColors.white,
-      textColor: themeColors.secondary,
-      dividerBg: "#f2f5f9",
-      menuItemColor: "#242E6F",
-      menuItemColorActive: themeColors.primary,
-      menuItemBg: "transparent",
-      menuItemBgActive: "#f2f5f9",
-      menuItemIconColor: lighten(themeColors.secondary, 0.3),
-      menuItemIconColorActive: themeColors.primary,
-      menuItemHeadingColor: darken(themeColors.secondary, 0.3),
+    excel: {
+      default: "#0F8953",
+      hover: "#13784B",
     },
-  },
-  alpha: {
-    white: {
-      5: alpha(themeColors.white, 0.02),
-      10: alpha(themeColors.white, 0.1),
-      30: alpha(themeColors.white, 0.3),
-      50: alpha(themeColors.white, 0.5),
-      70: alpha(themeColors.white, 0.7),
-      100: themeColors.white,
+    delete: {
+      default: "#F65858",
+      hover: "#FFEBEB",
     },
-    trueWhite: {
-      5: alpha(themeColors.white, 0.02),
-      10: alpha(themeColors.white, 0.1),
-      30: alpha(themeColors.white, 0.3),
-      50: alpha(themeColors.white, 0.5),
-      70: alpha(themeColors.white, 0.7),
-      100: themeColors.white,
-    },
-    black: {
-      5: alpha(themeColors.black, 0.02),
-      10: alpha(themeColors.black, 0.1),
-      30: alpha(themeColors.black, 0.3),
-      50: alpha(themeColors.black, 0.5),
-      70: alpha(themeColors.black, 0.7),
-      100: themeColors.black,
-    },
-  },
-  secondary: {
-    lighter: lighten(themeColors.secondary, 0.85),
-    light: lighten(themeColors.secondary, 0.25),
-    main: themeColors.secondary,
-    dark: darken(themeColors.secondary, 0.2),
   },
   primary: {
-    lighter: lighten(themeColors.primary, 0.85),
-    light: lighten(themeColors.primary, 0.3),
-    main: themeColors.primary,
-    dark: darken(themeColors.primary, 0.2),
+    b900: "#07857F",
+    b700: "#009A93", //main
+    b500: "#32AEA9",
+    b300: "#5ABCB8",
+    b200: "#81C8C5",
+    b100: "#B7D8D6",
+    b050: "#DDF1F0",
   },
-  success: {
-    lighter: lighten(themeColors.success, 0.85),
-    light: lighten(themeColors.success, 0.3),
-    main: themeColors.success,
-    dark: darken(themeColors.success, 0.2),
+  secondary: {
+    b900: "#FCCB1C",
+    b600: "#FFD954",
+    b500: "#FFE91F",
+    b050: "#FFFAEA",
   },
-  warning: {
-    lighter: lighten(themeColors.warning, 0.85),
-    light: lighten(themeColors.warning, 0.3),
-    main: themeColors.warning,
-    dark: darken(themeColors.warning, 0.2),
+  bg: {
+    default: "#F2F5F5",
+    lightTeal: "#F5FCFB",
+    white: "#FFFFFF",
   },
-  error: {
-    lighter: lighten(themeColors.error, 0.85),
-    light: lighten(themeColors.error, 0.3),
-    main: themeColors.error,
-    dark: darken(themeColors.error, 0.2),
+  gray: {
+    b900: "#434C4C", //high
+    b700: "#677272", //medium
+    b600: "#8C9695", //low
+    b400: "#B0BABA", //placeholder
+    b300: "#CCD2D2",
+    b000: "#FFFFFF",
   },
-  info: {
-    lighter: lighten(themeColors.info, 0.85),
-    light: lighten(themeColors.info, 0.3),
-    main: themeColors.info,
-    dark: darken(themeColors.info, 0.2),
+  border: {
+    section: "#C4CECC",
+    lightGreen: "#EDF9F8",
+    light: "#F1F5F4",
+    default: "#DCE6E5",
+  },
+  shadow: {
+    drop: "0px 0px 15px rgba(0, 0, 0, 0.15)",
+    section: "0px 0px 15px rgba(196, 197, 206, 0.15)",
   },
 };
+
 const lightTheme = createTheme({
   spacing: (value) => value * 5,
   shape: {
@@ -132,9 +79,7 @@ const lightTheme = createTheme({
       white: "#FFF",
     },
     primary: {
-      light: colors.primary.light,
-      main: colors.primary.main,
-      dark: colors.primary.dark,
+      main: colors.primary.b700,
     },
     secondary: {
       light: "#FFE91F",
