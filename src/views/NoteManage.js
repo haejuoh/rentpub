f4eef1aaea3a132699b5c9dab88414524f8c64ef
kmingu <rkdalsrnapm@gmail.com>
2022-05-30 14:59:12 +0900

    비고(내부관리용), 비고(차량준비용) 수정

--- src/views/NoteManage.js
+++ src/views/NoteManage.js
@@ -8,10 +8,11 @@ import {
   TextField,
   Typography,
   IconButton,
+  TextareaAutosize,
+  Box,
 } from "@mui/material";
 import { SubTitle } from "../components/Title";
 import { Section } from "../components/Section";
-import SelectModal from "../components/SelectModal";
 import { ReactComponent as IconChevronDown } from "../assets/images/icons/chevron-down.svg";
 
 const TitleToggle = {
@@ -21,8 +22,55 @@ const TitleToggle = {
   tooltip: false,
 };
 
-const TextArea = (props) => {
-  const { line, focus, type } = { ...props };
+const ReadArea = (props) => {
+  const { children } = { ...props };
+  return (
+    <Typography
+      variant="b1"
+      style={{
+        height: "120px",
+        backgroundColor: "#F5FCFB",
+        width: "100%",
+        padding: "10px",
+      }}
+    >
+      {children}
+    </Typography>
+  );
+};
+
+export const TextArea = (props) => {
+  const { line, focus } = { ...props };
+  return (
+    <>
+      <TextareaAutosize
+        minRows={line}
+        placeholder="내용을 입력해주세요"
+        style={{
+          width: "100%",
+          border: "1px solid #f3f4f4",
+          fontFamily: "Pretendard,sans-serif",
+          padding: "9px 12px",
+        }}
+      />
+      <Grid container alignItems="baseline">
+        <Grid item xs={6}>
+          <Typography variant="b2">2022.12.22 14:09:52 홍길동</Typography>
+        </Grid>
+        <Grid item xs={6} textAlign="right">
+          <>
+            <Button color="primary" variant="outlined">
+              작성완료
+            </Button>
+          </>
+        </Grid>
+      </Grid>
+      <Divider type="item15" />
+    </>
+  );
+};
+export const TextAreaList = (props) => {
+  const { line, focus, readOnly } = { ...props };
   const [noteState, toggleNote] = React.useState({
     open: false,
     line: line,
@@ -38,33 +86,26 @@ const TextArea = (props) => {
   };
   return (
     <>
-      <TextField
-        id="outlined-multiline-static"
-        placeholder="내용을 입력해주세요"
-        multiline
-        rows={noteState.line}
-        focused={focus}
-      />
       <Grid container alignItems="baseline">
-        <Grid item xs={6}>
+        <Grid item xs={9}>
+          <Typography variant="b2">안녕하세요 SK렌터카입니다.</Typography>
+        </Grid>
+        <Grid item xs={3} textAlign="right">
           <Typography variant="b2">2022.12.22 14:09:52 홍길동</Typography>
+          <IconButton
+            color="primary"
+            variant="outlined"
+            onClick={toggle}
+            style={{ marginLeft: 15 }}
+          >
+            <IconChevronDown style={setStyle} />
+          </IconButton>
         </Grid>
-        <Grid item xs={6} textAlign="right">
-          {type === "main" && (
-            <>
-              <SelectModal />
-              <Button color="primary" variant="outlined">
-                작성완료
-              </Button>
-            </>
-          )}
-          {type === "list" && (
-            <IconButton color="primary" variant="outlined" onClick={toggle}>
-              <IconChevronDown style={setStyle} />
-            </IconButton>
-          )}
-        </Grid>
+        {noteState.open ? (
+          <ReadArea>안녕하세요 SK렌터카입니다.</ReadArea>
+        ) : null}
       </Grid>
+      <Divider type="item15" />
     </>
   );
 };
@@ -76,16 +117,17 @@ const NoteManage = () => {
         <SubTitle titleData={TitleToggle} />
         <Divider type="item15" />
         <Stack direction="column" spacing={10} justifyContent="space-between">
-          <TextArea line={5} type="main" />
-          <TextArea line={1} type="list" />
-          <IconButton
+          <TextArea line={6} />
+          <TextAreaList line={1} />
+          <TextAreaList line={1} />
+          <Button
             color="sub"
             variant="contained"
             size="small"
             endIcon={<IconChevronDown />}
           >
             더보기
-          </IconButton>
+          </Button>
         </Stack>
       </Section>
     </>
--- src/views/NoteVehicle.js
+++ src/views/NoteVehicle.js
@@ -8,9 +8,12 @@ import {
   TextField,
   Typography,
   IconButton,
+  TextareaAutosize,
+  Box,
 } from "@mui/material";
 import { SubTitle } from "../components/Title";
 import { Section } from "../components/Section";
+import { TextArea, TextAreaList } from "./NoteManage";
 import { ReactComponent as IconChevronDown } from "../assets/images/icons/chevron-down.svg";
 
 const TitleToggle = {
@@ -20,77 +23,28 @@ const TitleToggle = {
   tooltip: false,
 };
 
-const TextArea = (props) => {
-  const { line, focus, readOnly, type, children } = { ...props };
-  const [noteState, toggleNote] = React.useState({
-    open: false,
-    line: line,
-  });
-  const toggle = () => {
-    toggleNote({
-      open: !noteState.open,
-      line: noteState.open ? line : 5,
-    });
-  };
-  return (
-    <>
-      <TextField
-        id="outlined-multiline-static"
-        placeholder="내용을 입력해주세요"
-        defaultValue={children}
-        multiline
-        rows={noteState.line}
-        InputProps={{
-          readOnly: readOnly,
-        }}
-      />
-      <Grid container alignItems="baseline">
-        <Grid item xs={12} textAlign="right">
-          <Typography variant="b2">2022.12.22 14:09:52 홍길동</Typography>
-          {type === "main" && (
-            <Button color="primary" variant="outlined">
-              작성완료
-            </Button>
-          )}
-          {type === "list" && (
-            <IconButton color="primary" variant="outlined" onClick={toggle}>
-              <IconChevronDown
-                style={{
-                  transform: `scaleY(${noteState.open ? -1 : 1})`,
-                }}
-              />
-            </IconButton>
-          )}
-        </Grid>
-      </Grid>
-    </>
-  );
-};
-
-const NoteVehicle = () => {
+const NoteManage = () => {
   return (
     <>
       <Section>
         <SubTitle titleData={TitleToggle} />
         <Divider type="item15" />
         <Stack direction="column" spacing={10} justifyContent="space-between">
-          <TextArea line={5} type="main" />
-          <TextArea line={1} readOnly type="list">
-            안녕하세요. SK렌터카입니다.
-          </TextArea>
-          <TextArea line={1} readOnly type="list"></TextArea>
-          <IconButton
+          <TextArea line={6} />
+          <TextAreaList line={1} />
+          <TextAreaList line={1} />
+          <Button
             color="sub"
             variant="contained"
             size="small"
             endIcon={<IconChevronDown />}
           >
             더보기
-          </IconButton>
+          </Button>
         </Stack>
       </Section>
     </>
   );
 };
 
-export default NoteVehicle;
+export default NoteManage;
