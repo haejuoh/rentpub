921cf4994aa509d47bea121fb70e424ea37dd1b1
조정옥 <evagreen12011@gmail.com>
2022-06-09 15:27:54 +0900

    badge - result item type custom 추가

--- src/components/skr/SkrChip.js
+++ src/components/skr/SkrChip.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { styled } from "@mui/styles";
 import { Chip } from "@mui/material";
+import { theme } from "../../assets/style/customTheme";
 /**
  * @typedef {import('@mui/material').ChipProps} ChipProps
  */
@@ -8,17 +10,52 @@ import { Chip } from "@mui/material";
  * @type { React.FC<ChipProps> }
  */
 
-const SkrChip = ({ label, color, variant, size, disabled, children }) => {
+const ChipStyled = styled(Chip)(({ variant }) => {
   return (
-    <Chip
+    variant === "result" && {
+      "&.MuiChip-root": {
+        minHeight: "24px",
+        padding: "2px 4px 2px 6px",
+        backgroundColor: `${theme.palette.sub.dark}`,
+        borderRadius: "2px",
+        "& .MuiChip-label": {
+          paddingLeft: "0",
+          paddingRight: "5px",
+          color: `${theme.palette.primary.light}`,
+          fontSize: `${theme.typography.b1.fontSize}`,
+          "& + svg path": {
+            fill: `${theme.palette.primary.light}`,
+          },
+        },
+      },
+    }
+  );
+});
+
+const SkrChip = ({
+  label,
+  color,
+  variant,
+  size,
+  disabled,
+  clickable,
+  deleteIcon,
+  onDelete,
+  children,
+}) => {
+  return (
+    <ChipStyled
       label={label}
       color={color}
       variant={variant}
       size={size}
       disabled={disabled}
+      clickable={clickable}
+      deleteIcon={deleteIcon}
+      onDelete={onDelete}
     >
       {children}
-    </Chip>
+    </ChipStyled>
   );
 };
 
