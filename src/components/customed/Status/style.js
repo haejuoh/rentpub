1a5a3ae4eec8cc6f1d49abcc7e5b2e2890ce22bc
조정옥 <evagreen12011@gmail.com>
2022-06-14 17:31:56 +0900

    status check 수정

--- src/components/customed/Status/style.js
+++ src/components/customed/Status/style.js
@@ -2,54 +2,6 @@ import React from "react";
 import { styled } from "@mui/styles";
 import { theme } from "../../../assets/style/customTheme";
 
-// const StatusCheckWrap = styled("div")(({ theme, status }) => {
-//   return status === "success"
-//     ? {
-//         "&": {
-//           display: "inline-block",
-//           width: "24px",
-//           height: "24px",
-//           "& svg": {
-//             width: "100%",
-//             height: "100%",
-//             "& path": {
-//               fill: `${theme.palette.success.main}`,
-//             },
-//           },
-//         },
-//       }
-//     : status === "danger"
-//     ? {
-//         "&": {
-//           display: "inline-block",
-//           width: "24px",
-//           height: "24px",
-//           "& svg": {
-//             width: "100%",
-//             height: "100%",
-//             "& path": {
-//               fill: `${theme.palette.danger.main}`,
-//             },
-//           },
-//         },
-//       }
-//     : status === "cancel"
-//     ? {
-//         "&": {
-//           display: "inline-block",
-//           width: "24px",
-//           height: "24px",
-//           "& svg": {
-//             width: "100%",
-//             height: "100%",
-//             "& path": {
-//               fill: `${theme.palette.cancel.main}`,
-//             },
-//           },
-//         },
-//       }
-//     : {};
-// });
 const StatusCheckWrap = styled("div")(({ theme, status }) => {
   return {
     "&": {
