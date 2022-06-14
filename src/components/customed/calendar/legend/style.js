cc5dd19573c1c0ca7f05236f18c17176f42e7fa7
j2hyun <w2gus3263@gmail.com>
2022-06-14 17:28:19 +0900

    legend styling edit

--- src/components/customed/calendar/legend/style.js
+++ src/components/customed/calendar/legend/style.js
@@ -12,26 +12,14 @@ const ChipBox = styled("div")(() => ({
   width: "18px",
   height: "18px",
   padding: "0.1rem",
-  "& > span": {
+}));
+const Chip = styled("span")(({ theme, status }) => {
+  return {
     display: "inline-block",
     width: "100%",
     height: "100%",
     borderRadius: "100%",
-  },
-}));
-const Chip = styled("span")(({ theme, status }) => {
-  return status === "ready"
-    ? { backgroundColor: `${theme.palette.ready.main}` }
-    : status === "success"
-    ? { backgroundColor: `${theme.palette.success.main}` }
-    : status === "warning"
-    ? { backgroundColor: `${theme.palette.warning.main}` }
-    : status === "done"
-    ? { backgroundColor: `${theme.palette.done.main}` }
-    : status === "danger"
-    ? { backgroundColor: `${theme.palette.danger.main}` }
-    : status === "cancel"
-    ? { backgroundColor: `${theme.palette.cancel.main}` }
-    : {};
+    backgroundColor: theme.palette[status].main,
+  };
 });
 export { Item, ChipBox, Chip };
