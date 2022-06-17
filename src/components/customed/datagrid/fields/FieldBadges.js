2f2d3f89d2a0e1ef612da13e78218346e716d6f0
조정옥 <evagreen12011@gmail.com>
2022-06-17 15:59:36 +0900

    console log 삭제

--- src/components/customed/datagrid/fields/FieldBadges.js
+++ src/components/customed/datagrid/fields/FieldBadges.js
@@ -2,7 +2,6 @@ import React from "react";
 import SkrChip from "../../../skr/SkrTooltip";
 
 const FieldBadges = (params) => {
-  console.log(params);
   switch (params.value) {
     case "사용":
       return <SkrChip label={params.value} color="success" />;
